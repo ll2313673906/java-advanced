71edf9385bfd18e0a22672cf26d20498562e2292
ll2313673906 <3340624204@qq.com>
2019-03-25 07:30:41 +0800

    Swing - FlowLayout流式布局

--- collection-study/src/com/soft1841/Season.java
+++ collection-study/src/com/soft1841/Season.java
@@ -1,4 +1,9 @@
 package com.soft1841;
+/**
+ * Swing - FlowLayout流式布局
+ * @author 刘恋
+ * 2019.3.25
+ */
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,7 +19,7 @@ public class Season extends JFrame {
         container.add(new JButton("B.白雪纷纷何所似,撒盐空中差可似。"));
         container.add(new JButton("C.接天莲叶无穷碧，映日荷花别样红。"));
         container.add(new JButton("D.竹外桃花三两枝,春江水暖鸭先知。"));
-        setSize(500,500);
+        setSize(500,200);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setVisible(true);
         setResizable(false);
