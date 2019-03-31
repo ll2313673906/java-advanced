19cefa268cdb6055d4818fa9a07821e6427b83f1
ll2313673906 <3340624204@qq.com>
2019-04-01 05:51:47 +0800

    遍历文件夹

---
+++ swing-study/src/com/soft1841/FolderTest.java
@@ -0,0 +1,25 @@
+package com.soft1841;
+/**
+ * 遍历文件夹
+ * @author刘恋
+ * 2019.4.1
+ */
+
+import java.io.File;
+
+public class FolderTest {
+    public static void main(String[] args) {
+        // 要遍历的路径
+        String path = "F:\\360Downloads";
+        // 获取其fil对象
+        File file = new File(path);
+        // 遍历path下的文件和目录，放在file数组中
+        File[] fs = file.listFiles();
+        // 遍历File数组
+        for (File f:fs){
+            // 若非目录(即文件）则打印
+            if (!f.isDirectory())
+                System.out.println(f);
+        }
+    }
+}
