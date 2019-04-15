f5281b0e51ce56fc0392a1315c91d433629c16a2
ll2313673906 <3340624204@qq.com>
2019-04-16 07:14:10 +0800

    绘制组件

--- thread-study/src/com/soft1841/thread/timer/ComponentPaintFrame.java
+++ thread-study/src/com/soft1841/thread/timer/ComponentPaintFrame.java
@@ -12,23 +12,22 @@ import java.io.File;
 import java.io.IOException;
 
 public class ComponentPaintFrame extends JFrame {
-
     public ComponentPaintFrame(){
         init();
-        setTitle("组件绘制");
+        setTitle("绘制组件");
         setSize(800,600);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public void init(){
-        // 创建JPanel对象的同时将背景图绘制上去
-        JPanel jPanel = new JPanel(){
-            protected void paintComponent(Graphics g){
-                try{
-                    Image bg = ImageIO.read(new File("D:/fengzi.jpg"));
-                    g.drawImage(bg,0,0,getWidth(),getHeight(),null);
-                }catch (IOException e){
+    private void init() {
+        //创建JPanel对象的同时，将背景图绘制进去
+        JPanel jPanel = new JPanel() {
+            protected void paintComponent(Graphics g) {
+                try {
+                    Image bg = ImageIO.read(new File("F:\\360Downloads\\2129.jpg"));
+                    g.drawImage(bg, 0, 0, getWidth(), getHeight(),null);
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -37,21 +36,22 @@ public class ComponentPaintFrame extends JFrame {
         add(jPanel);
         JButton jButton = new JButton(){
             protected void paintComponent(Graphics g){
-                try{
-                    //获取图片绘制在当前的对象上
-                    Image bg = ImageIO.read(new File("D:/2.jpg"));
+                try {
+                    //获取图片并绘制在当前对象上
+                    Image bg = ImageIO.read(new File("F:\\flower\\sun.jpg"));
                     g.drawImage(bg,0,0,getWidth(),getHeight(),null);
                     g.drawString("按钮",55,25);
-                  }catch (IOException e){
+                }catch (IOException e){
                     e.printStackTrace();
                 }
             }
         };
-        jButton.setPreferredSize(new Dimension(150,50) );
+        jButton.setPreferredSize(new Dimension(150,50));
+        //不获取焦点
         jButton.setFocusPainted(false);
-        // 背景透明
-
-        // 无边框
+        //背景透明
+        jButton.setContentAreaFilled(false);
+        //无边框
         jButton.setBorderPainted(false);
         jPanel.add(jButton);
     }
